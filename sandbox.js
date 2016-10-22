f055ec3316b7a38c5bcf90a810dba5a0cb7f8fc0
gnkatchmar <gunk55@msn.com>
2016-10-22 16:49:01 -0700

    more sandboxing, getting closer

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,51 @@
+//palette-bitmap.bmp
+
 var fs = require('fs');
 
-function createNewBuffer (buffer, callback){
+// function createNewBuffer (buffer, callback){
+
+//   fs.readFile(buffer, function (err, buffer ) {
+//     console.log(buffer);
+//     var newBuffer = Buffer.from(buffer);
+//     console.log(newBuffer);
+//     callback(err, newBuffer);
+//   });
+// }
+
+// function createFileHeaderInfo (newBuffer, callback) {
+
+//   //if bitsPerPixel = 24, it is non-palette
+//   const startPalette = newBuffer.readInt32LE(10);
+//   console.log(startPalette);  // 54 for non-palette; 1078 for palette
+//   const bitsPerPixel = newBuffer.readInt16LE(28);
+//   console.log(bitsPerPixel); // 24 for non-palette; 8 for palette
+
+
+//   callback(null, startPalette);
+  
+// }
+
+// createNewBuffer('data/non-palette-bitmap.bmp', function(err, newBuffer) {
+//   createFileHeaderInfo(newBuffer, function() {
+
+//   });
+// });
+
+function transformNP (buffer, callback) {
 
   fs.readFile(buffer, function (err, buffer ) {
     console.log(buffer);
+    const bitsPerPixel = buffer.readInt16LE(28);
+    console.log(bitsPerPixel); // 24 for non-palette; 8 for palette
+    const startPalette = buffer.readInt32LE(10);
+    console.log(startPalette);  // 54 for non-palette; 1078 for palette
     var newBuffer = Buffer.from(buffer);
     console.log(newBuffer);
     callback(err, newBuffer);
   });
+
+  
+
 }
 
-function getOffset (newBuffer, callback) {
-
-	size = newBuffer.slice(2,6);
-	console.log(size);  // 66 75 = 26,229
-  offset = newBuffer.slice(10,14);
-  console.log(offset); // = 36 = 54
-  callback(null, offset); // = 36 = 54
-}
-
-createNewBuffer('data/non-palette-bitmap.bmp', function(err, newBuffer) {
-  getOffset(newBuffer, function() {
-
-  });
-});
-
-
-
+allInOne('data/non-palette-bitmap.bmp', function(err, newBuffer)

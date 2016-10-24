5771941278d33195c53bbda45e9093cd019253d6
gnkatchmar <gunk55@msn.com>
2016-10-23 20:04:04 -0700

    wrestling with line endings in lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,6 @@ module.exports = {
             "error",
             "tab"
         ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
         "quotes": [
             "error",
             "double"
